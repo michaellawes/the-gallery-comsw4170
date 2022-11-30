77ccfb3087e58aa151272c7f44cdd225667e5e2a
Delilah Beverly <delilahbeverly@Delilahs-MacBook-Pro.local>
2022-11-30 17:39:10 -0500

    updated frontend

--- frontend/src/components/FeedComponents/FeedPanel.js
+++ frontend/src/components/FeedComponents/FeedPanel.js
@@ -4,16 +4,16 @@ import './FeedPanel.css';
 function FeedPanel(props) {
   return (
     <div className="feed-panel">
-      <div className="feed-panel fp-top-left">
+      <div className="feed-panel-obj fp-top-left">
         <FeedGallery src={props.srcTopLeft} title={props.titleTopLeft}/>
       </div>
-      <div className="feed-panel fp-top-right">
+      <div className="feed-panel-obj fp-top-right">
         <FeedGallery src={props.srcTopRight} title={props.titleTopRight}/>
       </div>
-      <div className="feed-panel fp-bottom-left">
+      <div className="feed-panel-obj fp-bottom-left">
         <FeedGallery src={props.srcBottomLeft} title={props.titleBottomLeft}/>
       </div>
-      <div className="feed-panel fp-bottom-right">
+      <div className="feed-panel-obj fp-bottom-right">
         <FeedGallery src={props.srcBottomRight} title={props.titleBottomRight}/>
       </div>
     </div>
