fba14db29ad59095f6b67f80dcc01f6261a1de31
Michael Lawes <mjalawes@gmail.com>
2022-12-07 21:59:25 -0500

    Refactor index.js and app.js to use routes for linking between pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import PageHeader from './components/HeaderComponents/PageHeader';
-import './App.css';
-import Feed from './pages/Feed';
-
+import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Feed from './pages/Feed.js';
+import Login from './pages/Login.js'
+import PageHeader from './components/HeaderComponents/PageHeader.js'
 function App() {
-  const getProfileIcon = () => {
-    return "";
-  }
-
   return (
-    <div className="App">
-      <div>
-        <PageHeader src={getProfileIcon} />
-        <Feed profileIcon={""}/>
+    <Router>
+      <div className="App">
+        <PageHeader/>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Feed />} />
+              <Route path="/sign-in" element={<Login />} />
+            </Routes>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    </Router>
+  )
 }
-
-export default App;
+export default App
--- frontend/src/components/HeaderComponents/PageHeader.js
+++ frontend/src/components/HeaderComponents/PageHeader.js
@@ -3,16 +3,31 @@ import HeaderNotificationBell from "./HeaderNotificationBell";
 import HeaderSearchIcon from "./HeaderSearchIcon";
 import HeaderHomeButton from "./HeaderHomeButton";
 import HeaderTitle from "./HeaderTitle";
+import { Link } from 'react-router-dom'
 import './PageHeader.css';
-
+      
 function PageHeader(props) {
   return (
     <div className="page-header">
-      <HeaderHomeButton />
-      <HeaderTitle />
-      <HeaderNotificationBell />
-      <HeaderSearchIcon />
-      <HeaderProfileIcon {...props.src}/>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={'/'}>
+            <HeaderHomeButton/>
+          </Link>
+        </div>
+        <div className="container">
+          <HeaderTitle/>
+        </div>
+        <div className="container">
+          <HeaderNotificationBell />
+        </div>
+        <div className="container">
+          <HeaderSearchIcon />
+        </div>
+        <div className="container">
+          <HeaderProfileIcon {...props.src}/>
+        </div>
+      </nav>
     </div>
   );
 }
