64e9b2074b69add7f82c85bd39f0a701277f1f56
Delilah Beverly <delilahbeverly@Delilahs-MacBook-Pro.local>
2022-12-11 16:11:58 -0500

    added images

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -2,6 +2,12 @@ import PageHeader from "../components/HeaderComponents/PageHeader";
 import FeedPanel from "../components/FeedComponents/FeedPanel";
 import './Feed.css';
 import oop from '../static/oop.JPEG';
+import image2 from '../static/image2.png';
+import image3 from '../static/image3.png';
+import image4 from '../static/image4.png';
+import image5 from '../static/image5.png';
+
+
 
 function Feed(props) {
   const getProfileIcon = () => {
@@ -11,14 +17,27 @@ function Feed(props) {
   return (
     <div className="Feed">
       <PageHeader src={getProfileIcon} />
-      <FeedPanel srcTopLeft={oop} titleTopLeft="Top Left Gallery"
-      srcTopRight={"srcTopRight.jpg"}
+      <FeedPanel srcTopLeft={image2} titleTopLeft="Top Left Gallery"
+      srcTopRight={image3}
       titleTopRight="Top Right Gallery"
-      srcBottomLeft="srcBottomLeft.jpg"
+      srcBottomLeft={image4}
       titleBottomLeft="Bottom Left Gallery"
-      srcBottomRight="srcBottomRight.jpg"
+      srcBottomRight={image5}
       titleBottomRight="Bottom Right Gallery"/>
+
+<div class="container">
+<div class="row">
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+    <div class="w-100"></div>
+    <div class="col">Column</div>
+    <div class="col">Column</div>
+  </div>
+  </div>
+
     </div>
+
+    
   );
 }
 
