4956f8b380f719ccce069fb6cc87b70ac64eb0e8
Michael Lawes <mjalawes@gmail.com>
2022-12-20 15:42:38 -0500

    update notifications

--- frontend/src/components/NotificationsComponents/Notification.js
+++ frontend/src/components/NotificationsComponents/Notification.js
@@ -1,12 +1,27 @@
 import './Notification.css';
+import MapIcon from "@mui/icons-material/Map";
+import ComputerIcon from '@mui/icons-material/Computer';
+import ColorLensIcon from "@mui/icons-material/ColorLens";
 
 function Notification(props) {
   return (
-    <div className="notification">
-      <img src={props.icon} alt={"Notification Icon"}></img>
-      <span>{props.title}</span>
-      <span>{props.description}</span>
-      <img src={props.artist} alt="Artist"></img>
+    <div className={props.class}>
+      <div className="icon inner-icon">
+        {props.icon === 0 && <MapIcon style={{ color: "black" }} />}
+        {props.icon === 1 && <ComputerIcon style={{ color: "black" }} />}
+        {props.icon === 2 && <ColorLensIcon style={{ color: "black" }} />}
+      </div>
+      <div className="inner-text text-container">
+        <div className="text-item title">
+          <span>{props.title}</span>
+        </div>
+        <div className="text-item description">
+          <span>{props.description}</span>
+        </div>
+      </div>
+      <div className="inner-icon artist">
+        <img src={props.artist} alt="Artist"></img>
+      </div>
     </div>
   );
 }
--- frontend/src/pages/Notifications.js
+++ frontend/src/pages/Notifications.js
@@ -1,33 +1,42 @@
-import './Notifications.css';
-import Notification from '../components/NotificationsComponents/Notification';
+import "./Notifications.css";
+import Notification from "../components/NotificationsComponents/Notification";
+import cal from "../static/cal.png";
 
-function Notifications(props) {
+function Notifications() {
   return (
-    <div className="Notifications">
-     <div className='notification-array'>
-      <Notification
-        title="Upcoming Physical Exhibition"
-        description="New physical exhibition"
-        src=""
-        artist=""
-      />
-     </div>
-     <div className='notification-array'>
-      <Notification
-        title="Upcoming Virtual Exhibition"
-        description="New virtual exhibition"
-        src=""
-        artist=""
-      />
-     </div>
-     <div className='notification-array'>
-      <Notification
-        title="New Art"
-        description="New ART!!!"
-        src=""
-        artist=""
-      />
-     </div>
+    <div className="notifications">
+      <div className="noti-container">
+        <div className="noti-item">
+          <Notification
+            title="Upcoming Physical Exhibition"
+            description="Frances is hosting a new physical exhibition on 23/12/2022 in Uris Hall Columbia University."
+            icon={0}
+            artist={"Frances"}
+            class={"inner-container blue"}
+          />
+        </div>
+        <div className="noti-item">
+          <Notification
+            title="Upcoming Virtual Exhibition"
+            description="Romero is hosting a new virtual exhibition on 18/12/2022"
+            icon={1}
+            artist={"Romero"}
+            class={"inner-container red"}
+          />
+        </div>
+        <div className="noti-item">
+          <Notification
+            title="New Art"
+            description="Frances has put out new art. Check it out!"
+            icon={2}
+            artist={"Frances"}
+            class={"inner-container green"}
+          />
+        </div>
+        <div className="noti-cal">
+          <img src={cal} alt="Calendar"></img>
+        </div>
+      </div>
     </div>
   );
 }
